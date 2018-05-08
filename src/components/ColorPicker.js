c8eed11fa2a74a58fb9d2ab5a9d5d02028004deb
Tyson Battistella <tysonbattistella@gmail.com>
2018-05-08 11:34:44 -0700

    save previous colors and allow them to be reused

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {inject, observer} from 'mobx-react';
 import styled from 'styled-components';
-import {Heading, Divider, Text, ButtonTransparent} from 'rebass';
+import {Heading, Divider, Text, ButtonTransparent, Flex, Box, Circle} from 'rebass';
 import {ChromePicker} from 'react-color';
 import {colors} from '../theme';
 import Panel from './Panel';
@@ -49,20 +49,36 @@ export default class ColorPicker extends React.Component {
     this.setState({displayColorPicker: false});
   }
 
+  handlePreviousColorClick(color){
+    return () => this.handleColorChange({hex: color});
+  }
+
   render (){
-    const {currentColor} = this.props.colorStore;
+    const {currentColor, previousColors} = this.props.colorStore;
     return (
       <Panel p={2} accent={currentColor}>
         <Heading fontSize={[2, 3, 4]}>The Pixels .Club</Heading>
         <Text>The Pixels .Club is a public pixel canvas. To participate, select a color below and place your pixels on the artboard </Text>
         <Divider/>
-        <Button onClick={this.handleDisplayClick.bind(this)}>Select a Color</Button>
-        { this.state.displayColorPicker ? (
-          <Popover>
-            <Cover onClick={this.handleCoverClick.bind(this)}/>
-            <ChromePicker color={currentColor} disableAlpha={true} onChangeComplete={this.handleColorChange.bind(this)}/>
-          </Popover>
-        ) : null}
+        <Flex flexWrap={'wrap'}>
+          <Box w={[1, 1/2, 1/3, 1/4]}>
+            <Button onClick={this.handleDisplayClick.bind(this)}>Select a Color</Button>
+            { this.state.displayColorPicker ? (
+              <Popover>
+                <Cover onClick={this.handleCoverClick.bind(this)}/>
+                <ChromePicker color={currentColor} disableAlpha={true} onChangeComplete={this.handleColorChange.bind(this)}/>
+              </Popover>
+            ) : null}
+          </Box>
+          {previousColors.map((c) => (
+            <Box key={c} my={1}>
+              <Circle
+                onClick={this.handlePreviousColorClick.bind(this)(c)}
+                bg={c}>{currentColor === c ? 'A' : ''}</Circle>
+            </Box>
+          ))}
+
+        </Flex>
       </Panel>
     )
   }
--- src/stores/colorStore.js
+++ src/stores/colorStore.js
@@ -2,9 +2,23 @@ import {observable, action} from 'mobx';
 
 class ColorStore {
   @observable currentColor;
+  @observable previousColors = [];
 
   @action setColor(color){
     this.currentColor = color;
+    const previousColors = [...this.previousColors];
+
+    if (previousColors.includes(color)) {
+      const idx = previousColors.indexOf(color);
+      previousColors.splice(idx, 1);
+    }
+    previousColors.unshift(color);
+
+    this.previousColors = previousColors;
+
+    // if(this.previousColors.length > 10){
+    //   this.previousColors.splice(-1,1);
+    // }
   }
 
   @action resetColor(){
