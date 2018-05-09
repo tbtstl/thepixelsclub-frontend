24431f9f331f76b0b0886ff9654eb542586a4c18
Tyson Battistella <tysonbattistella@gmail.com>
2018-05-09 00:15:14 -0700

    :)

--- src/stores/colorStore.js
+++ src/stores/colorStore.js
@@ -1,10 +1,17 @@
 import {observable, action} from 'mobx';
 
+// Just for the heck of it
+function changeThemeColor(color){
+  const metaThemeColor = document.querySelector("meta[name=theme-color]");
+  metaThemeColor.setAttribute("content", color);
+}
+
 class ColorStore {
   @observable currentColor;
   @observable previousColors = [];
 
   @action setColor(color){
+    changeThemeColor(color);
     this.currentColor = color;
   }
 
